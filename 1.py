0270d597255d925e94509c33ce38be51903dadd5
novin <temka.mondrunov@mail.ru>
2024-11-29 21:07:49 +0300

    3

--- 1.py
+++ 1.py
@@ -11,3 +11,27 @@ class Book:
 
     def discount(self, percentage):
         self.price *= (1 - percentage / 100)
+        
+class Author:
+    def init(self, name, bio):
+        self.name = name
+        self.bio = bio
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+class Customer:
+    def init(self, name, email, balance):
+        self.name = name
+        self.email = email
+        self.balance = balance
+        self.purchases = []
+
+    def buy_book(self, book):
+        if book.is_available and self.balance >= book.price:
+            self.balance -= book.price
+            self.purchases.append(book)
+            book.is_available = False
+        else:
+            raise ValueError("Not enough balance or book is unavailable.")
