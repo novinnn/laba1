f55c513dbfd87ff0992fe20c02ffff4904e8529c
novin <temka.mondrunov@mail.ru>
2024-11-29 21:01:12 +0300

    second

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
-print('hello world')
+from datetime import datetime
+
+class Book:
+    def init(self, title, author, price, genre, publication_date):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.genre = genre
+        self.publication_date = publication_date
+        self.is_available = True
+
+    def discount(self, percentage):
+        self.price *= (1 - percentage / 100)
